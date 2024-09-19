6c18ea4333d65558d322c889b16bbacaf533d420
mateusnovo <mpnmateus@gmail.com>
2024-09-19 13:31:01 -0300

    Usando a @TestMethodOrderer by DisplayName - comentado

--- src/test/java/EscolhendoAOrdemTeste.java
+++ src/test/java/EscolhendoAOrdemTeste.java
@@ -1,26 +1,31 @@
 import org.junit.jupiter.api.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+//@TestMethodOrder(MethodOrderer.DisplayName.class)
 public class EscolhendoAOrdemTeste {
 
+    //@DisplayName("Teste que valida se o usuário foi criado")
     @Order(4)
     @Test
     void validaFluxoA() {
         Assertions.assertTrue(true);
     }
 
+    //@DisplayName("B")
     @Order(3)
     @Test
     void validaFluxoB() {
         Assertions.assertTrue(true);
     }
 
+    //@DisplayName("C")
     @Order(2)
     @Test
     void validaFluxoC() {
         Assertions.assertTrue(true);
     }
 
+    //@DisplayName("D")
     @Order(1)
     @Test
     void validaFluxoD() {
