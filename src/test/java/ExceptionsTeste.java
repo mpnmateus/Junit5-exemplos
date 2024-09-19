1fb432aa82f76d150aedd48a0beca7e41d3ce1ed
mateusnovo <mpnmateus@gmail.com>
2024-09-19 13:22:29 -0300

    Adicionando um caso de teste em exceptions

--- src/test/java/ExceptionsTeste.java
+++ src/test/java/ExceptionsTeste.java
@@ -14,4 +14,14 @@ public class ExceptionsTeste {
 
         Assertions.assertDoesNotThrow(() -> transferenciaEntreContas.transfere(contaOrigem, contaDestino, 20));
     }
+
+    @Test
+    void validarCenarioDeExcecaoNaTransferencia2 () {
+        Conta contaOrigem = new Conta("123456", 0);
+        Conta contaDestino = new Conta("456548", 100);
+
+        TransferenciaEntreContas transferenciaEntreContas = new TransferenciaEntreContas();
+
+        Assertions.assertThrows (IllegalArgumentException.class, () -> transferenciaEntreContas.transfere(contaOrigem, contaDestino, -1));
+    }
 }
