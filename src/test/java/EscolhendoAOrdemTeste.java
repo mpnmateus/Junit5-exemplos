a0342cbc816b127823b8c5a1a2cdf6037a8aaddb
mateusnovo <mpnmateus@gmail.com>
2024-09-19 13:28:34 -0300

    Usand TestMethodOrder

--- src/test/java/EscolhendoAOrdemTeste.java
+++ src/test/java/EscolhendoAOrdemTeste.java
@@ -1,24 +1,27 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class EscolhendoAOrdemTeste {
 
-
+    @Order(4)
     @Test
     void validaFluxoA() {
         Assertions.assertTrue(true);
     }
 
+    @Order(3)
     @Test
     void validaFluxoB() {
         Assertions.assertTrue(true);
     }
 
+    @Order(2)
     @Test
     void validaFluxoC() {
         Assertions.assertTrue(true);
     }
 
+    @Order(1)
     @Test
     void validaFluxoD() {
         Assertions.assertTrue(true);
