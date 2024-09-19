ba8d68c65ae8d13364653d173c6f7eafafd8a2f7
mateusnovo <mpnmateus@gmail.com>
2024-09-19 13:25:38 -0300

    construindo um caso para ordernar testes

---
+++ src/test/java/EscolhendoAOrdemTeste.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EscolhendoAOrdemTeste {
+
+
+    @Test
+    void validaFluxoA() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void validaFluxoB() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void validaFluxoC() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void validaFluxoD() {
+        Assertions.assertTrue(true);
+    }
+
+
+
+}
